4bd74f48380ec706f23c5264d0a5b6d7874db251
JavierAmdR <javieramador_bcnvj1ma1920@cev.com>
2020-10-08 18:55:18 +0200

    Copiado proyecto y añadido script de movimiento básico

---
+++ KnightErrantPrototype/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector3 movementVector;
+
+    private float moveSide;
+    private float moveFront;
+
+    [SerializeField] private float speed;
+    [SerializeField] private CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementVector = new Vector3(moveSide, 0, moveFront);
+        characterController.Move(movementVector * Time.deltaTime * speed);
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void OnMove(InputValue value) 
+    {
+        Debug.Log("Move");
+        moveSide = value.Get<Vector2>().x;
+        moveFront = value.Get<Vector2>().y;
+    }
+}
