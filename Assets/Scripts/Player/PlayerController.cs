37ec7895c2350aebb64ed587bb77063a5f96490e
JavierAmdR <javieramador_bcnvj1ma1920@cev.com>
2020-10-10 21:27:43 +0200

    Lógica de ataque añadida

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,31 +11,82 @@ public class PlayerController : MonoBehaviour
     private float moveSide;
     private float moveFront;
 
+    [SerializeField] private float attackRange = 0.5f;
     [SerializeField] private float speed;
-    [SerializeField] private CharacterController characterController;
 
-    // Start is called before the first frame update
+    private bool attacking = false;
+    
+    private CharacterController characterController;
+    [SerializeField] private Transform attackObject;
+
+    [SerializeField] private LayerMask enemyLayer;
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        attackObject = GetComponentInChildren<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        movementVector = new Vector3(moveSide, 0, moveFront);
-        characterController.Move(movementVector * Time.deltaTime * speed);
+    {        
+       Move();
+       if (attacking == true) 
+        {
+            Attack();
+        }
     }
+    
 
-    private void FixedUpdate()
-    {
-        
-    }
+    //INPUTS
 
+    //Move input
     void OnMove(InputValue value) 
     {
         Debug.Log("Move");
         moveSide = value.Get<Vector2>().x;
         moveFront = value.Get<Vector2>().y;
     }
+
+    //Attack input
+    void OnAttack()
+    {
+        Debug.Log("Attack");
+        attacking = true;
+    }
+
+    //FUNCTIONS
+
+    //Move function
+    void Move() 
+    {
+        movementVector = new Vector3(moveSide, 0, moveFront);
+        characterController.Move(movementVector * Time.deltaTime * speed);
+    }
+
+    //Attack function
+    void Attack() 
+    {
+        //HACER QUE LA FUNCIÓN ACTIVE LA ANIMACIÓN Y DESPUES QUE ESTA LLAME A OTRA FUNCIÓN DE ATAQUE
+        //CAMBIAR
+        Collider[] hitEnemies = Physics.OverlapSphere(attackObject.position, attackRange, enemyLayer);
+        foreach (Collider enemy in hitEnemies) 
+        {
+            Debug.Log("Hit " + enemy.name);
+        }
+        attacking = false;
+        //CAMBIAR
+    }
+
+    //EDITOR
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackObject == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackObject.position, attackRange);
+    }
+
+
 }
