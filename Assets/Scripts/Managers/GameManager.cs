a954880d8920e4c1e0f34e8f6fa36756df386d52
JavierAmdR <javieramador_bcnvj1ma1920@cev.com>
2020-12-06 21:44:22 +0100

    Transición entre escenas

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-
+    public Animator transition;
     public static GameManager current;
+    public float transitionTime = 1f;
 
     void Awake()
     {
+        transition = GameObject.Find("Crossfade").GetComponent<Animator>();
         DontDestroyOnLoad(this);
     }
     void Start()
@@ -19,6 +22,36 @@ public class GameManager : MonoBehaviour
     public void ChangeTimeScale (float timeScale) 
     {
         Time.timeScale = timeScale;
+    }
+
+    public void StartGame() 
+    {
+        LoadScene(1);
+    }
+
+    public void ExitGame() 
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene(int newScene) 
+    {
+        StartCoroutine(LoadCrossfade(newScene));
+    }
+
+    IEnumerator LoadCrossfade(int sceneIndex) 
+    {
+        transition.SetTrigger("CrossfadeStart");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadSceneAsync(sceneIndex);
+        while(SceneManager.GetActiveScene().buildIndex != sceneIndex) 
+        {
+            yield return null;
+        }
+        if (SceneManager.GetActiveScene().buildIndex == sceneIndex) 
+        {
+            transition.SetTrigger("CrossfadeEnd");
+        }
     }
     
 }
---
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private GameObject mainMenu;
+    private GameObject settingsMenu;
+
+    private void Awake()
+    {
+        mainMenu = GameObject.Find("MainMenu");
+        //settingsMenu = GameObject.Find("");
+    }
+}
