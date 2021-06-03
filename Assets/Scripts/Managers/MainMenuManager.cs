adc05f3c2e8f8d5cc9885c2116488718897f72b9
RogerFont120 <rogerfont_bcnvj1ma1920@cev.com>
2021-06-03 18:08:32 +0200

    Cinematica inicial añadida

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class MainMenuManager : MonoBehaviour
 
     public void StartGameButton() 
     {
-        GameManager.current.StartGame();
+        SceneManager.LoadScene("Cinematic");
+        //GameManager.current.StartGame();
     }
 }
---
+++ Assets/Videos/Cinematic_Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Cinematic_Manager : MonoBehaviour
+{
+    public VideoPlayer cinematic;
+    private bool LoadedScene = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cinematic.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cinematic.isPlaying == true)
+        {
+            Debug.Log("Cinematic playing");
+        }
+        else
+        {
+            if (LoadedScene == false)
+            {
+                GameManager.current.StartGame();
+                LoadedScene = true;
+                Debug.Log("Scene Loaded");
+            }
+        }
+    }
+}
