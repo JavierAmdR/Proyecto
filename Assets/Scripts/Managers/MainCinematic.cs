d9c6d1e67b604c016bd323af6dcc3168a544f237
EricValls2B <ericvalls_bcnvj1ma1920@cev.com>
2021-06-04 15:37:48 +0200

    implementacion boton skip cinematica no finalizado

---
+++ Assets/Scripts/Managers/MainCinematic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainCinematic : MonoBehaviour
+{
+    public GameObject skipPanel;
+    public void SkipButton() 
+    {
+        Debug.Log("he presionado skip");
+        SceneManager.LoadScene("HUB");
+        //GameManager.current.StartGame();
+    }
+}
